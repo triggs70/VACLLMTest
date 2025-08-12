db0cc25590eae268d9ce503e0dd794c8474ae43a
Tyler Riggs <tmrr86@mun.ca>
2025-08-12 15:38:06 -0230

    chore: emptt unreal engine project

---
+++ Source/VACLabLLMTest/VACLabLLMTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VACLabLLMTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VACLabLLMTest, "VACLabLLMTest" );
