2c822d0dfcb51c615e2b827588992eaf1910c70b
Tyler Riggs <tmrr86@mun.ca>
2025-08-13 13:13:52 -0230

    Feature: add URotateOverTimeComponent and attached to BP_Cube

---
+++ Source/VACLabLLMTest/Private/RotateOverTimeComponent.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RotateOverTimeComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+URotateOverTimeComponent::URotateOverTimeComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.
+	// You can turn these features off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
+
+// Called when the game starts
+void URotateOverTimeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void URotateOverTimeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* Owner = GetOwner();
+	if (Owner)
+	{
+		FRotator RotationThisFrame = FRotator(0.f, DegreesPerSecond * DeltaTime, 0.f);
+		Owner->AddActorLocalRotation(RotationThisFrame);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("URotateOverTimeComponent: Owner is null!"));
+	}
+}
---
+++ Source/VACLabLLMTest/Public/RotateOverTimeComponent.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RotateOverTimeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VACLABLLMTEST_API URotateOverTimeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	URotateOverTimeComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="VACLabLLMTest")
+	float DegreesPerSecond = 90.f;
+};
